dc800e45c2d8dcc79fdf7138ca32f3d8c9cb6a87
Abdul Khan <akhan@lcs.on.ca>
2020-11-23 12:34:59 -0500

    Made the picker be bound to an integer so the selections work properly

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
+struct Conversions: Identifiable {
+    var id = UUID()
+    var input: String
+    var output: String
+    
+}
+
 struct ContentView: View {
+    
+    @State private var unitConversion = 1
+    
+    let conversions = ["Cm->Inches","Km->Miles","Celsius->Farenheit"]
+    @State private var chosenConverter = "Cm->Inches"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Type", selection: $unitConversion) {
+                        ForEach(0..<conversions.count) {
+                            Text("\(self.conversions[$0])")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("Conversion: \(unitConversion)")
+                }
+            }
+            .navigationTitle("Unit Converter")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
